8a87afa703486c0820fee4241d930ac18b27ebd7
John Van Enk <vanenkj@gmail.com>
2013-05-15 16:51:53 -0400

    Output the pretty-printed version of the parsed data.

--- main.hs
+++ main.hs
@@ -4,12 +4,13 @@ import qualified Data.Text.IO as TO
 
 import Text.Parsec
 import Data.Cauterize.Parser
+import Text.PrettyPrint.HughesPJClass
 
 main :: IO ()
 main = do
   d <- TO.readFile fname
   case parse parseCauterize fname d of
     Left e -> print e
-    Right v -> print v
+    Right v -> print $ pPrint v
   where
     fname = "example.scm"
