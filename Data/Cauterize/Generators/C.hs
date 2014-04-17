6d694b1e6759ecc8361c92a0673ddb9c95050ab9
John Van Enk <vanenkj@gmail.com>
2014-04-17 00:09:20 -0400

    Output something so we know that at least that is working.

--- Data/Cauterize/Generators/C.hs
+++ Data/Cauterize/Generators/C.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Data.Cauterize.Generators.C ( gen ) where
 
 import Data.Cauterize.Specification
 import qualified Text.PrettyPrint as P
+import Text.PrettyPrint ((<+>))
 import qualified Data.Text as T
 
 gen :: Spec -> P.Doc
-gen = undefined
+gen (Spec (SpecName n) (SpecVersion v) (SpecHash h) rs) =
+  P.text (T.unpack n) <+> P.text (T.unpack v) <+> P.text (T.unpack h)
