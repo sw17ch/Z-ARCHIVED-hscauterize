d158e43964c7f6ae139b40e0d51f5a8badac2630
John Van Enk <vanenkj@gmail.com>
2014-04-17 00:08:53 -0400

    Flesh out rules

--- Data/Cauterize/Specification.hs
+++ Data/Cauterize/Specification.hs
@@ -4,40 +4,56 @@
 
 module Data.Cauterize.Specification
   ( Spec(..)
+  , SpecName(..)
+  , SpecVersion(..)
+  , SpecHash(..)
   , fromSchema
   ) where
 
 import Data.Cauterize.Schema
 import Data.Data
 import Data.String
-import Data.Text (Text, unpack)
+import qualified Data.Text as T
 import qualified Data.Map as M
 
 fromSchema :: Schema -> Spec
 fromSchema (Schema (SchemaName n) (SchemaVersion v) rs) =
-  Spec
-    (SpecName n)
-    (SpecVersion v)
-    (fromRules rs)
+  let s = Spec
+            (SpecName n)
+            (SpecVersion v)
+            (SpecHash $ mkSpecHash s)
+            (fromRules rs)
+  in s
 
-fromRules _ = M.empty
+mkSpecHash :: Spec -> T.Text
+mkSpecHash _ = "INVALID"
+
+fromRules :: M.Map TypeName SchemaRule -> M.Map TypeName SpecRule
+fromRules src = undefined
+
+fromRule :: SchemaRule -> SpecRule
+fromRule = undefined
 
 data Spec = Spec
   { specName :: SpecName
   , specVersion :: SpecVersion
+  , specHash :: SpecHash
   , specRules :: M.Map TypeName SpecRule
   }
 
-newtype SpecName = SpecName Text
+newtype SpecName = SpecName T.Text
   deriving (Show, Data, Typeable, IsString)
 
-newtype SpecVersion = SpecVersion Text
+newtype SpecVersion = SpecVersion T.Text
   deriving (Show, Data, Typeable, IsString)
 
-data SpecRule = SpecScalar
-              | SpecEnumeratino
-              | SpecFixed
-              | SpecBounded
-              | SpecComposite
-              | SpecGroup
+newtype SpecHash = SpecHash T.Text
+  deriving (Show, Data, Typeable, IsString)
+
+data SpecRule = SpecScalar TypeName TypeName
+              | SpecEnumeration TypeName [EnumValue] BuiltIn
+              | SpecFixed TypeName TypeName Const
+              | SpecBounded TypeName TypeName Const BuiltIn
+              | SpecComposite TypeName [Field]
+              | SpecGroup TypeName [Field] BuiltIn
   deriving (Show, Data, Typeable)
